17fd9754ffddc2f796beb5c938ee7a05e33b6f7d
Xin Feng <1244200081@qq.com>
2022-07-01 16:29:55 +0800

    feat Message Component get log

--- src/Message/demos/index1.tsx
+++ src/Message/demos/index1.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Button from '../../Button';
 import Message from '..';
+import usePageListener from '../../_util/hooks/usePageListener';
 
 export default function index1() {
+  process.env.NODE_ENV === 'production' && usePageListener('Message');
+
   return (
     <Button type="primary" handleClick={() => Message.info('This is an info message!')}>
       Open Message
