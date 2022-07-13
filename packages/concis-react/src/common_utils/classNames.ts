795bcbba3863a84241b8db4edcc1eb29b5b1c6bd
Xin Feng <1244200081@qq.com>
2022-07-14 07:07:47 +0800

    feat class doc

--- packages/concis-react/src/common_utils/classNames.ts
+++ packages/concis-react/src/common_utils/classNames.ts
@@ -1,7 +1,6 @@
 type classNamesArg = Array<any>;
 
 export default function (...args: classNamesArg): string {
-  console.log(args);
   let classNames: any = [];
   args.forEach((item) => {
     classNames.push(item);
