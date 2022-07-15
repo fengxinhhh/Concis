782261ab3cc343db9d53264ad197fbd791076e06
Xin Feng <1244200081@qq.com>
2022-07-16 02:21:54 +0800

    feat version update script file

---
+++ scripts/replace-version-in-pc.ts
@@ -0,0 +1,25 @@
+const fs = require('fs-extra');
+const path = require('path');
+const { version } = require('../packages/concis-react/package.json');
+
+const fileText = fs.readFileSync(
+  path.join(__dirname, '../packages/concis-react/package.json'),
+  'utf-8',
+);
+
+const replacedVersion = fileText.split('\n').map((t) => {
+  if (/"version":/.test(t)) {
+    let newVersion = version.split('.');
+    newVersion[newVersion.length - 1] = Number(newVersion[newVersion.length - 1]) + 1;
+    console.log(newVersion.join('.'));
+    return `  "version":"${newVersion.join('.')}",`;
+  }
+  return t;
+});
+
+fs.outputFile(
+  path.resolve(__dirname, '../packages/concis-react/package.json'),
+  replacedVersion.join('\n'),
+).then(() => {
+  console.log('替换 package.json 中的 version 成功！'); // eslint-disable-line
+});
