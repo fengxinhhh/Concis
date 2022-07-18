103ccfafb89dc41f745c297059925b8692d3ba49
Xin Feng <1244200081@qq.com>
2022-07-19 01:27:29 +0800

    feat .umi path

---
+++ .umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/apple/Desktop/Concis/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
+});
+
+export { plugin };
