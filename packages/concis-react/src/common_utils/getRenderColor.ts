07400adad5a14b72749571f6eed9125cfe6d1276
Xin Feng <1244200081@qq.com>
2022-07-21 02:23:23 +0800

    feat datepicker dark theme:

--- packages/concis-react/src/common_utils/getRenderColor.ts
+++ packages/concis-react/src/common_utils/getRenderColor.ts
@@ -1,6 +1,6 @@
 //根据暗黑模式、普通模式、用户自定义全局色判断最终使用颜色
 
-const getRenderColor = (isDark: boolean, globalColor: string) => {
+const getRenderColor = (isDark: boolean, globalColor: string | undefined): string => {
   if (globalColor) {
     return globalColor;
   }
