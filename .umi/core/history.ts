d4d3d0210585ab41bd5ee4f23a8a23a71178c076
guowanzi <8750640+guowanzi@user.noreply.gitee.com>
2022-07-21 18:19:37 +0800

    chore(DatePicker):dumi自动构建的

--- .umi/core/history.ts
+++ .umi/core/history.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import { createHashHistory, History } from '/Users/apple/Desktop/Concis/node_modules/@umijs/runtime';
+import { createHashHistory, History } from 'F:/font/Project/my-git-pr/Concis/node_modules/@umijs/runtime';
 
 let options = {
   "basename": "/"
--- .umi/core/plugin.ts
+++ .umi/core/plugin.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import { Plugin } from '/Users/apple/Desktop/Concis/node_modules/@umijs/runtime';
+import { Plugin } from 'F:/font/Project/my-git-pr/Concis/node_modules/@umijs/runtime';
 
 const plugin = new Plugin({
   validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
