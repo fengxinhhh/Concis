c171ba9c6e13ab6e62ef2c947e379cece7f57854
Xin Feng <1244200081@qq.com>
2022-07-23 22:42:09 +0800

    update  .gitignore

--- .umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/apple/Desktop/Concis/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
-});
-
-export { plugin };
