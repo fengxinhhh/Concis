d3d7a636a68ca40074be4ba4b0db63b50c844636
Xin Feng <1244200081@qq.com>
2022-08-04 15:58:05 +0800

    fix: Button Component mobile show bug

--- packages/concis-react/src/Button/demos/index5.tsx
+++ packages/concis-react/src/Button/demos/index5.tsx
@@ -5,7 +5,7 @@ export default function ButtonDemo5() {
   const buttonStyle = { margin: '8px' };
 
   return (
-    <div style={{ display: 'flex' }}>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
       <Button type="primary" style={buttonStyle} dashed>
         虚线按钮
       </Button>
--- packages/concis-react/src/Button/demos/index6.tsx
+++ packages/concis-react/src/Button/demos/index6.tsx
@@ -5,7 +5,7 @@ export default function ButtonDemo6() {
   const buttonStyle = { margin: '8px' };
 
   return (
-    <div style={{ display: 'flex' }}>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
       <Button type="primary" style={buttonStyle} loading>
         加载按钮
       </Button>
