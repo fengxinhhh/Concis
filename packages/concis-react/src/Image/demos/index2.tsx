ee9387eecfad85ed9ccfaf4b183d15d585c3a147
chengongwei <1391581728@qq.com>
2022-08-09 17:58:07 +0800

    feature(Image): add Image component

---
+++ packages/concis-react/src/Image/demos/index2.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from '..';
+import usePageListener from '../../common_utils/hooks/usePageListener';
+
+export default function ImageDemo1() {
+  process.env.NODE_ENV === 'production' && usePageListener('Image');
+
+  const fitList = ['fill', 'contain', 'cover', 'none', 'scale-down']
+
+  return (
+    <>
+      {fitList.map((fit: any, index) => (
+        <Image
+          src="http://concis.org.cn/images/swiper-img5.webp"
+          width="200px"
+          height="200px"
+          style={{  marginRight: '20px' }}
+          fit={fit}
+          key={index}
+        />
+      ))}
+    </>
+  );
+}
