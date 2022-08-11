ee2c380e2b299f2299a99bad103d7f5eeacb81b0
chengongwei <1391581728@qq.com>
2022-08-11 14:53:55 +0800

    feat：update Image

--- packages/concis-react/src/Image/demos/index2.tsx
+++ packages/concis-react/src/Image/demos/index2.tsx
@@ -5,7 +5,7 @@ import usePageListener from '../../common_utils/hooks/usePageListener';
 export default function ImageDemo1() {
   process.env.NODE_ENV === 'production' && usePageListener('Image');
 
-  const fitList = ['fill', 'contain', 'cover', 'none', 'scale-down']
+  const fitList = ['fill', 'contain', 'cover', 'none', 'scale-down'];
 
   return (
     <>
@@ -14,7 +14,7 @@ export default function ImageDemo1() {
           src="http://concis.org.cn/images/swiper-img5.webp"
           width="200px"
           height="200px"
-          style={{  marginRight: '20px' }}
+          style={{ marginRight: '20px' }}
           fit={fit}
           key={index}
         />
