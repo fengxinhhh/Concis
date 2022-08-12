0f9a6bef24c5a5f60ecb5d2a93dfd7a241942319
JokerWon <1595672103@qq.com>
2022-08-12 10:47:42 +0800

    perf(backtop): 完善文档

--- packages/concis-react/src/BackTop/interface.ts
+++ packages/concis-react/src/BackTop/interface.ts
@@ -1,12 +1,19 @@
 import { ReactNode } from 'react';
 
 interface BackTopProps {
+  /**
+   * @description 自定义按钮内容
+   */
   children?: ReactNode;
   /**
    * @description 回到顶部所需时间(ms)
    * @default 450
    */
   duration?: number;
+  /**
+   * @description 需要监听滚动事件的目标元素
+   * @default window
+   */
   target?: () => HTMLElement | Window | Document;
   /**
    * @description 滚动距离大于该参数才会显示
