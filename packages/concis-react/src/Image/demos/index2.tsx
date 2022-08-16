25ccba6e52ba17709877b2a934678fb20dbf63d9
Xin Feng <1244200081@qq.com>
2022-08-16 14:16:01 +0800

    feat: Image snapshots test

--- packages/concis-react/src/Image/demos/index2.tsx
+++ packages/concis-react/src/Image/demos/index2.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import Image from '..';
-import usePageListener from '../../common_utils/hooks/usePageListener';
 
 export default function ImageDemo1() {
-  process.env.NODE_ENV === 'production' && usePageListener('Image');
-
   const fitList = ['fill', 'contain', 'cover', 'none', 'scale-down'];
 
   return (
--- packages/concis-react/src/Image/demos/index4.tsx
+++ packages/concis-react/src/Image/demos/index4.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import Image from '..';
-import usePageListener from '../../common_utils/hooks/usePageListener';
 
 export default function ImageDemo1() {
-  process.env.NODE_ENV === 'production' && usePageListener('Image');
-
   return (
     <Image
       src="http://concis.org.cn/images/swiper-img5.webp"
