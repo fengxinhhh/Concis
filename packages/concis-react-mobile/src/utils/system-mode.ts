da42c03d2d85252c4f5f0d50bb72f2fbe0282fca
747383481 <747383481@qq.com>
2022-08-26 11:02:25 +0800

    feat: 移动端自动模式支持系统切换时自动切换深色\浅色模式

--- packages/concis-react-mobile/src/utils/system-mode.ts
+++ packages/concis-react-mobile/src/utils/system-mode.ts
@@ -1,23 +1,61 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
 
-export const getSystemMode = () => {
-  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)');
+type SystemMode = {
+  darkMode: MediaQueryList;
+  theme: string;
 };
 
-export const getSystemTheme = () => {
-  const darkMode = getSystemMode();
+export const getSystemMode = (): SystemMode => {
+  const darkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)');
 
-  return darkMode && darkMode.matches ? 'dark' : 'light';
+  const theme = darkMode && darkMode.matches ? 'dark' : 'light';
+
+  return {
+    darkMode,
+    theme,
+  };
 };
 
-export const useSetSystemTheme = (theme?: string) => {
+export const useSetSystemTheme = (value?: string) => {
+  const rootRef = useRef(document.documentElement);
+  const darkModeRef = useRef<SystemMode>(getSystemMode());
+
+  const [theme, setTheme] = useState('');
+
+  // 绑定addEventListener的回调函数，需要useCallback包裹防止重新生成函数
+  const darkModeChange = useCallback((e: MediaQueryListEvent) => {
+    if (e.matches) {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  }, []);
+
   useEffect(() => {
-    if (!theme) return;
+    if (!theme || !rootRef.current) return;
 
-    const root = document.documentElement;
-
-    const value = theme === 'auto' ? getSystemTheme() : theme;
-
-    root.setAttribute('data-ccsm-theme', value);
+    rootRef.current.setAttribute('data-ccsm-theme', theme);
   }, [theme]);
+
+  useEffect(() => {
+    if (!value) return;
+
+    if (value === 'auto') {
+      darkModeRef.current.darkMode.addEventListener('change', darkModeChange);
+
+      setTheme(darkModeRef.current.theme);
+    } else {
+      darkModeRef.current.darkMode?.removeEventListener('change', darkModeChange);
+
+      setTheme(value);
+    }
+  }, [value]);
+
+  useEffect(() => {
+    return () => {
+      if (!darkModeRef.current?.darkMode) return;
+
+      darkModeRef.current.darkMode?.removeEventListener('change', darkModeChange);
+    };
+  }, []);
 };
